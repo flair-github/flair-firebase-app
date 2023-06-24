54456d0af233053244c2fc8f4bd9fea86cfe043e
Ezzat Chamudi <echamudi@outlook.com>
2023-06-25 01:44:38 +0700

    Update firestore structure

--- types/firebaseStructure.ts
+++ types/firebaseStructure.ts
@@ -1,3 +1,5 @@
+import { Timestamp } from 'firebase/firestore'
+
 interface FSCollections {
   [collectionName: string]: {
     [documentId: string]: {
@@ -10,46 +12,71 @@ interface FSCollections {
 export interface FirestoreStruct extends FSCollections {
   user_data: {
     [userId: string]: {
-      document: {
-        userId: string
-        userName: string
-      }
+      document: DocUserData
       collections: {}
     }
   }
   flow_data: {
     [flowDataId: string]: {
-      document: {
-        ownerUserId: string
-        flowJson: string
-      }
+      document: DocFlowData
       collections: {}
     }
   }
-  flow_execution_requests: {
-    [flowExecutionRequestId: string]: {
-      document: {
-        date: any
-        executorUserId: string
-        flowDataId: string
-        flowJson: string
-      }
+  flow_exec_requests: {
+    [flowExecRequestId: string]: {
+      document: DocFlowData
       collections: {}
     }
   }
-  flow_execution_results: {
-    [flowExecutionRequestId: string]: {
-      document: {
-        date: any
-        flowExecutionRequestId: string
-        executorUserId: string
-        flowDataId: string
-        /** history */
-        flowJson: string
+  flow_exec_results: {
+    [flowExecRequestId: string]: {
+      document: DocFlowExecResult
+      collections: {}
+    }
+  }
+}
 
-        resultData: string
-      }
-      collections: {}
-    }
-  }
+export interface DocRoot {
+  docExists: boolean
+  createdTimestamp: Timestamp
+  updatedTimestamp: Timestamp
+}
+
+export interface DocUserData extends DocRoot {
+  /** Document Id */
+  userId: string
+
+  userName: string
+  userPhotoUrl: string
+}
+
+export interface DocFlowData extends DocRoot {
+  /** Document Id */
+  flowDataId: string
+
+  ownerUserId: string
+  flowDataJson: string
+}
+
+export interface DocFlowExecRequest extends DocRoot {
+  /** Document Id */
+  flowExecRequestId: Timestamp
+
+  executorUserId: string
+  flowDataId: string
+
+  requestTimestamp: Timestamp
+  flowDataJson: string
+}
+
+export interface DocFlowExecResult extends DocRoot {
+  /** Document Id */
+  flowExecResultId: string
+
+  flowExecRequestId: string
+  executorUserId: string
+
+  resultTimestamp: Timestamp
+  resultStatus: string
+  resultData: any
 }
