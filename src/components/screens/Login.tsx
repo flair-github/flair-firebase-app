3bde651c989b40e29cb4fbdad3bbcfd17f900810
Ezzat Chamudi <echamudi@outlook.com>
2023-06-25 01:18:08 +0700

    Update login page

--- src/components/screens/Login.tsx
+++ src/components/screens/Login.tsx
@@ -17,17 +17,19 @@ function PageLogin() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
-      <div className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-96 max-w-md">
-       <div className="flex justify-center">
-        <img src="images/flair-ai.svg" width={120} height={32} className="" />
-       </div>
-        <button onClick={handleGoogleLoginClick} className="relative mt-6 border rounded-md py-2 text-sm text-gray-800 bg-gray-100 hover:bg-gray-200">
+    <div className="flex min-h-screen flex-col items-center justify-center  bg-gray-300">
+      <div className="flex w-96 flex-col rounded-md bg-white px-4 py-8 shadow-md sm:px-6 md:px-8 lg:px-10">
+        <div className="flex justify-center">
+          <img src="images/flair-ai.svg" width={120} height={32} className="" />
+        </div>
+        <button
+          onClick={handleGoogleLoginClick}
+          className="relative mt-6 rounded-md border bg-gray-100 py-2 text-sm text-gray-800 hover:bg-gray-200">
           <span>Login with Google</span>
         </button>
       </div>
     </div>
-  );
+  )
 }
 
 export default PageLogin
