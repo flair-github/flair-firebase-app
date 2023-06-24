0e94c1a04d1e3f8d6b9c84110345d819ec60eb15
Ezzat Chamudi <echamudi@outlook.com>
2023-06-25 02:06:50 +0700

    Write user data

--- src/components/root/Main.tsx
+++ src/components/root/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Router } from '~/components/router/Router'
-import { setupFirebase } from '~/lib/firebase'
+import { db, setupFirebase } from '~/lib/firebase'
 import { useEffect } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useSetAtom } from 'jotai'
 import { atomUser } from '~/jotai/jotai'
+import { DocUserData } from 'Types/firebaseStructure'
+import { Timestamp, serverTimestamp } from 'firebase/firestore'
 
 function Main() {
   const setUser = useSetAtom(atomUser)
@@ -16,6 +18,31 @@ function Main() {
 
     const unsubscribe = onAuthStateChanged(auth, user => {
       setUser(user)
+
+      const uid = user?.uid
+
+      if (typeof uid === 'string') {
+        ;(async () => {
+          const userDataRef = db.collection('user_data').doc(uid)
+          const snap = await userDataRef.get()
+
+          if (snap.exists) {
+            // If user data already exists, do nothing
+          } else {
+            // If it doesn't exist, create user data
+            const newUserData: DocUserData = {
+              createdTimestamp: serverTimestamp() as Timestamp,
+              docExists: true,
+              updatedTimestamp: serverTimestamp() as Timestamp,
+              userId: uid,
+              userName: user?.displayName || 'Flair User',
+              userPhotoUrl:
+                user?.photoURL || 'https://www.gravatar.com/avatar/' + user?.uid + '?d=retro&f=y',
+            }
+            await userDataRef.set(newUserData)
+          }
+        })()
+      }
     })
 
     return () => {
