44b418c11d64a37944e2091c35938bbda95d93e1
Ezzat Chamudi <echamudi@outlook.com>
2023-06-26 01:15:20 +0700

    Add update properties notes

--- types/firebaseStructure.ts
+++ types/firebaseStructure.ts
@@ -10,24 +10,28 @@ interface FSCollections {
 }
 
 export interface FirestoreStruct extends FSCollections {
+  // Rename to users
   user_data: {
     [userId: string]: {
       document: DocUserData
       collections: {}
     }
   }
+  // Rename to workflows
   flow_data: {
     [flowDataId: string]: {
       document: DocFlowData
       collections: {}
     }
   }
+  // Rename to workflow_requests
   flow_exec_requests: {
     [flowExecRequestId: string]: {
       document: DocFlowExecRequest
       collections: {}
     }
   }
+  // Rename to workflow_results
   flow_exec_results: {
     [flowExecResultId: string]: {
       document: DocFlowExecResult
@@ -52,31 +56,42 @@ export interface DocUserData extends DocRoot {
 
 export interface DocFlowData extends DocRoot {
   /** Document Id */
+  // TODO: Rename to workflowId
   flowDataId: string
 
   ownerUserId: string
 
   lastSaveTimestamp: Timestamp
 
+  // TODO: Rename to workflowTitle
   flowDataTitle: string
+  // TODO: Rename to workflowJson
   flowDataJson: string
 }
 
 export interface DocFlowExecRequest extends DocRoot {
   /** Document Id */
+  // TODO: Rename to workflowRequestId
   flowExecRequestId: string
 
   executorUserId: string
+  // TODO: workflowId
   flowDataId: string
 
   requestTimestamp: Timestamp
+
+  // TODO: Rename to workflowConfig
   flowDataJson: string
+
+  // TODO: Rename to generatedConfig
 }
 
 export interface DocFlowExecResult extends DocRoot {
   /** Document Id */
+  // workflowResultId
   flowExecResultId: string
 
+  // workflowRequestId
   flowExecRequestId: string
   executorUserId: string
 
