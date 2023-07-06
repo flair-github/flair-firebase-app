3ae1c0f8255dafd3cc31212b6c91b3e2cb9b5f47
Ezzat Chamudi <echamudi@outlook.com>
2023-07-07 00:02:54 +0700

    Add initial settings

--- src/components/screens/Settings.tsx
+++ src/components/screens/Settings.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react'
-import { Dialog } from '@headlessui/react'
-import { useRef, useState } from 'react'
-import { SignInButton } from '~/components/domain/auth/SignInButton'
-import { SignOutButton } from '~/components/domain/auth/SignOutButton'
-import { Head } from '~/components/shared/Head'
+import React from 'react'
 import { useAtomValue } from 'jotai'
+import { useEffect } from 'react'
 import { atomUserData } from '~/jotai/jotai'
-import { db } from '~/lib/firebase'
 
 function Settings() {
   const userData = useAtomValue(atomUserData)
@@ -16,11 +11,136 @@ function Settings() {
       return
     }
 
-    // TODO: Load own credentials document
     return () => {}
   }, [userData?.userId])
 
-  return <></>
+  return (
+    <div className="container mx-auto px-4">
+      <div className="max-w-xs form-control w-full">
+        <h1 className="mt-5 text-2xl font-bold">AWS</h1>
+
+        <label className="label">
+          <span className="label-text">AWS Access Key</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter AWS Access Key"
+          className="max-w-xs  input w-full border-gray-800"
+        />
+
+        <label className="label">
+          <span className="label-text">AWS Bucket Name</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter AWS Bucket Name"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <label className="label">
+          <span className="label-text">AWS Region Name</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter AWS Region Name"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <label className="label">
+          <span className="label-text">AWS Secret Key</span>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter AWS Secret Key"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <h1 className="mt-5 text-2xl font-bold">DeepGram</h1>
+
+        <label className="label">
+          <span className="label-text">DeepGram API Key</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter DeepGram API Key"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <h1 className="mt-5 text-2xl font-bold">Google Cloud</h1>
+
+        <label className="label">
+          <span className="label-text">Google Cloud Credentials</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Google Cloud Credentials"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <label className="label">
+          <span className="label-text">Google Storage Bucket</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Google Storage Bucket"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <h1 className="mt-5 text-2xl font-bold">OpenAI</h1>
+
+        <label className="label">
+          <span className="label-text">OpenAI API Key</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter OpenAI API Key"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <label className="label">
+          <span className="label-text">OpenAI Organization ID</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter OpenAI Organization ID"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <label className="label">
+          <span className="label-text">Owner User ID</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Owner User ID"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <h1 className="mt-5 text-2xl font-bold">Pine Cone</h1>
+
+        <label className="label">
+          <span className="label-text">Pine Cone API Key</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Pine Cone API Key"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <label className="label">
+          <span className="label-text">Pine Cone Environment</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter Pine Cone Environment"
+          className="max-w-xs input w-full border-gray-800"
+        />
+
+        <div>
+          <button className="btn my-5">Save</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Settings
