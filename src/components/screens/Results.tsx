eb18764fe053055d9def26b1ddf983529bae54cd
Ezzat Chamudi <echamudi@outlook.com>
2023-07-10 04:25:51 +0700

    Update results page

--- src/components/screens/Results.tsx
+++ src/components/screens/Results.tsx
@@ -4,6 +4,8 @@ import { SignOutButton } from '~/components/domain/auth/SignOutButton'
 import { Head } from '~/components/shared/Head'
 import FlowEditor from './FlowEditor'
 import { useNavigate } from 'react-router-dom'
+import { FaCloudDownloadAlt } from 'react-icons/fa'
+import { HiDocumentReport } from 'react-icons/hi'
 
 function PageResults() {
   const navigate = useNavigate()
@@ -15,7 +17,7 @@ function PageResults() {
           {/* head */}
           <thead>
             <tr>
-              <th />
+              <th>ID</th>
               <th>Workflow Name</th>
               <th>Request Date</th>
               <th>Total Time Taken</th>
@@ -26,83 +28,108 @@ function PageResults() {
           </thead>
           <tbody>
             <tr>
-              <td>1</td>
+              <td>#1831</td>
               <td>Customer Call Workflow</td>
               <td>2023-06-25 10:45:30</td>
               <td>15 minutes</td>
               <td>gpt-4</td>
-              <td>Accuracy: 98%, Hallucination 0.2%, Invalid Format: 0%, Latency: 500ms</td>
+              <td>Accuracy: 98%, Hallucination 1.2%, Invalid Format: 0%, Latency: 200ms</td>
               <td>
-                <a
-                  className="btn bg-slate-200"
-                  href="#"
-                  onClick={() => navigate('/result-details')}>
-                  Open
-                </a>
+                <div style={{ minWidth: 300 }}>
+                  <a
+                    className="btn m-1 bg-slate-200"
+                    href="#"
+                    onClick={() => navigate('/result-details')}>
+                    <HiDocumentReport /> Details
+                  </a>
+                  <a className="btn m-1 bg-slate-200" href="#" onClick={() => {}}>
+                    <FaCloudDownloadAlt /> Download
+                  </a>
+                </div>
               </td>
             </tr>
             <tr>
-              <td>2</td>
+              <td>#1830</td>
               <td>Customer Call Workflow</td>
               <td>2023-06-24 14:30:00</td>
               <td>8 minutes</td>
               <td>gpt-3.5</td>
-              <td>Accuracy: 95%, Hallucination 0.2%, Invalid Format: 2%, Latency: 700ms</td>
+              <td>Accuracy: 95%, Hallucination 2.4%, Invalid Format: 2%, Latency: 500ms</td>
               <td>
-                <a
-                  className="btn bg-slate-200"
-                  href="#"
-                  onClick={() => navigate('/result-details')}>
-                  Open
-                </a>
+                <div style={{ minWidth: 300 }}>
+                  <a
+                    className="btn m-1 bg-slate-200"
+                    href="#"
+                    onClick={() => navigate('/result-details')}>
+                    <HiDocumentReport /> Details
+                  </a>
+                  <a className="btn m-1 bg-slate-200" href="#" onClick={() => {}}>
+                    <FaCloudDownloadAlt /> Download
+                  </a>
+                </div>
               </td>
             </tr>
             <tr>
-              <td>3</td>
+              <td>#1829</td>
               <td>Supply Chain Optimization Workflow</td>
               <td>2023-06-24 12:00:00</td>
               <td>30 minutes</td>
               <td>Falcon</td>
               <td>Accuracy: 90%, Hallucination 0.2%, Invalid Format: 5%, Latency: 800ms</td>
               <td>
-                <a
-                  className="btn bg-slate-200"
-                  href="#"
-                  onClick={() => navigate('/result-details')}>
-                  Open
-                </a>
+                <div style={{ minWidth: 300 }}>
+                  <a
+                    className="btn m-1 bg-slate-200"
+                    href="#"
+                    onClick={() => navigate('/result-details')}>
+                    <HiDocumentReport /> Details
+                  </a>
+                  <a className="btn m-1 bg-slate-200" href="#" onClick={() => {}}>
+                    <FaCloudDownloadAlt /> Download
+                  </a>
+                </div>
               </td>
             </tr>
             <tr>
-              <td>4</td>
+              <td>#1828</td>
               <td>Product Launch Process Workflow</td>
               <td>2023-06-23 16:15:00</td>
               <td>45 minutes</td>
               <td>gpt-3.5</td>
               <td>Accuracy: 93%, Hallucination 0.2%, Invalid Format: 1%, Latency: 550ms</td>
               <td>
-                <a
-                  className="btn bg-slate-200"
-                  href="#"
-                  onClick={() => navigate('/result-details')}>
-                  Open
-                </a>
+                <div style={{ minWidth: 300 }}>
+                  <a
+                    className="btn m-1 bg-slate-200"
+                    href="#"
+                    onClick={() => navigate('/result-details')}>
+                    <HiDocumentReport /> Details
+                  </a>
+                  <a className="btn m-1 bg-slate-200" href="#" onClick={() => {}}>
+                    <FaCloudDownloadAlt /> Download
+                  </a>
+                </div>
               </td>
             </tr>
             <tr>
-              <td>5</td>
+              <td>#1827</td>
               <td>Financial Reporting</td>
               <td>2023-06-23 09:30:00</td>
               <td>20 minutes</td>
               <td>gpt-4</td>
               <td>Accuracy: 97%, Hallucination 0.2%, Invalid Format: 0%, Latency: 600ms</td>
               <td>
-                <a
-                  className="btn bg-slate-200"
-                  href="#"
-                  onClick={() => navigate('/result-details')}>
-                  Open
-                </a>
+                <div style={{ minWidth: 300 }}>
+                  <a
+                    className="btn m-1 bg-slate-200"
+                    href="#"
+                    onClick={() => navigate('/result-details')}>
+                    <HiDocumentReport /> Details
+                  </a>
+                  <a className="btn m-1 bg-slate-200" href="#" onClick={() => {}}>
+                    <FaCloudDownloadAlt /> Download
+                  </a>
+                </div>
               </td>
             </tr>
           </tbody>
