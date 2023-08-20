a778622ca33efe5f2f90a8050de5f7d3c72a16e9
Ezzat Chamudi <echamudi@outlook.com>
2023-08-21 01:40:17 +0900

    Fix result page

--- src/components/screens/ResultDetails.tsx
+++ src/components/screens/ResultDetails.tsx
@@ -276,7 +276,7 @@ function ResultDetails() {
         </div>
       )}
       {activeTab === 'result' && (
-        <div className="justify-left flex w-full overflow-y-scroll border [height:720px]">
+        <div className="justify-left flex w-full border">
           <div className="container max-w-200 p-5">
             <div className="px-4 sm:px-0">
               <h3 className="text-base font-semibold leading-7 text-gray-900">Workflow Result</h3>
