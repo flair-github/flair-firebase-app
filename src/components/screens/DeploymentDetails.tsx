f1a1e48b614824744a1b79a174ef1d314b3345e0
Miko <jherjati@gmail.com>
2023-10-04 10:13:44 +0700

    Deployment Details : adjust above table UI

--- src/components/screens/DeploymentDetails.tsx
+++ src/components/screens/DeploymentDetails.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import { BarChart } from './deployment/BarChart'
 import Results from './Results'
 import { PieChart } from './deployment/PieChart'
+import { GiCheckMark, GiChecklist, GiSandsOfTime } from 'react-icons/gi'
 
 interface DeploymentDetailsProps {}
 
@@ -20,83 +21,32 @@ const DeploymentDetails: React.FunctionComponent<DeploymentDetailsProps> = props
           </button>
         </div>
 
-        <div className="mb-8 mt-3 grid grid-cols-2 gap-3">
-          <div className="">
-            <h4 className="text-lg font-medium">Usage</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae voluptate fugiat
-              laudantium, ea consequuntur tenetur quasi necessitatibus repellat sapiente officiis
-            </p>
-          </div>
-          <div className="">
-            <h4 className="text-lg font-medium">Usage This Month</h4>
-            <article className="flex w-full justify-between">
-              <p>700.000</p>
-              <p>1.000.000</p>
-            </article>
-            <progress className="progress w-full" value="70" max="100" />
-          </div>
-        </div>
-
-        <div className="stats w-full rounded-md border shadow-sm">
+        <div className="stats mt-4 w-full rounded-md border shadow-sm">
           <div className="stat">
             <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+              <GiChecklist className="h-10 w-10" />
             </div>
-            <div className="stat-title">Usage this month</div>
-            <div className="stat-value">31K</div>
-            <div className="stat-desc">Jan 1st - Feb 1st</div>
+            <div className="stat-title">Data Processed</div>
+            <div className="stat-value">3776 rows</div>
+            <div className="stat-desc">3 GigaByte(s)</div>
           </div>
 
           <div className="stat">
             <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-                />
-              </svg>
-            </div>
-            <div className="stat-title">Usage Breakdown</div>
-            <div className="stat-value">4,200</div>
-            <div className="stat-desc">↗︎ 400 (22%)</div>
-          </div>
-
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                />
-              </svg>
+              <GiCheckMark className="h-10 w-10" />
             </div>
             <div className="stat-title">Success Rate</div>
-            <div className="stat-value">1,200</div>
-            <div className="stat-desc">↘︎ 90 (14%)</div>
+            <div className="stat-value">78 %</div>
+            <div className="stat-desc">From whole data</div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <GiSandsOfTime className="h-10 w-10" />
+            </div>
+            <div className="stat-title">Average Time</div>
+            <div className="stat-value">2.4 minutes</div>
+            <div className="stat-desc">For each item</div>
           </div>
         </div>
 
