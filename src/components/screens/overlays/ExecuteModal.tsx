d1d527d9a68c4aa655dcab9861e642d94c829494
Ezzat Chamudi <echamudi@outlook.com>
2024-01-25 10:58:14 +0900

    Add hourly frequency

--- src/components/screens/overlays/ExecuteModal.tsx
+++ src/components/screens/overlays/ExecuteModal.tsx
@@ -11,6 +11,8 @@ export const DeployModal = ({
   isDeploying: boolean
   dialogRef: any
 }) => {
+  const [currentEvery, setCurrentEvery] = useState('1d')
+
   return (
     <dialog ref={dialogRef} className="modal">
       <form method="dialog" className="modal-box">
@@ -20,41 +22,57 @@ export const DeployModal = ({
           <label className="label">
             <span className="label-text font-bold">Run Every</span>
           </label>
-          <select className="select mb-3 w-full border-black">
+          <select
+            className="select mb-3 w-full border-black"
+            value={currentEvery}
+            onChange={e => {
+              setCurrentEvery(e.target.value)
+            }}>
+            <option value={'1h'}>1h</option>
+            <option value={'2h'}>1d</option>
+            <option value={'4h'}>1d</option>
+            <option value={'8h'}>8h</option>
             <option value={'1d'}>1d</option>
             <option value={'2d'}>2d</option>
             <option value={'3d'}>3d</option>
             <option value={'7d'}>7d</option>
           </select>
-          <label className="label">
-            <span className="label-text font-bold">at</span>
-          </label>
-          <select className="select mb-3 w-full border-black">
-            <option value={'12am'}>12:00 am</option>
-            <option value={'1am'}>1:00 am</option>
-            <option value={'2am'}>2:00 am</option>
-            <option value={'3am'}>3:00 am</option>
-            <option value={'4am'}>4:00 am</option>
-            <option value={'5am'}>5:00 am</option>
-            <option value={'6am'}>6:00 am</option>
-            <option value={'7am'}>7:00 am</option>
-            <option value={'8am'}>8:00 am</option>
-            <option value={'9am'}>9:00 am</option>
-            <option value={'10am'}>10:00 am</option>
-            <option value={'11am'}>11:00 am</option>
-            <option value={'12pm'}>12:00 pm</option>
-            <option value={'1pm'}>1:00 pm</option>
-            <option value={'2pm'}>2:00 pm</option>
-            <option value={'3pm'}>3:00 pm</option>
-            <option value={'4pm'}>4:00 pm</option>
-            <option value={'5pm'}>5:00 pm</option>
-            <option value={'6pm'}>6:00 pm</option>
-            <option value={'7pm'}>7:00 pm</option>
-            <option value={'8pm'}>8:00 pm</option>
-            <option value={'9pm'}>9:00 pm</option>
-            <option value={'10pm'}>10:00 pm</option>
-            <option value={'11pm'}>11:00 pm</option>
-          </select>
+          {(currentEvery === '1d' ||
+            currentEvery === '2d' ||
+            currentEvery === '3d' ||
+            currentEvery === '7d') && (
+            <>
+              <label className="label">
+                <span className="label-text font-bold">at</span>
+              </label>
+              <select className="select mb-3 w-full border-black">
+                <option value={'12am'}>12:00 am</option>
+                <option value={'1am'}>1:00 am</option>
+                <option value={'2am'}>2:00 am</option>
+                <option value={'3am'}>3:00 am</option>
+                <option value={'4am'}>4:00 am</option>
+                <option value={'5am'}>5:00 am</option>
+                <option value={'6am'}>6:00 am</option>
+                <option value={'7am'}>7:00 am</option>
+                <option value={'8am'}>8:00 am</option>
+                <option value={'9am'}>9:00 am</option>
+                <option value={'10am'}>10:00 am</option>
+                <option value={'11am'}>11:00 am</option>
+                <option value={'12pm'}>12:00 pm</option>
+                <option value={'1pm'}>1:00 pm</option>
+                <option value={'2pm'}>2:00 pm</option>
+                <option value={'3pm'}>3:00 pm</option>
+                <option value={'4pm'}>4:00 pm</option>
+                <option value={'5pm'}>5:00 pm</option>
+                <option value={'6pm'}>6:00 pm</option>
+                <option value={'7pm'}>7:00 pm</option>
+                <option value={'8pm'}>8:00 pm</option>
+                <option value={'9pm'}>9:00 pm</option>
+                <option value={'10pm'}>10:00 pm</option>
+                <option value={'11pm'}>11:00 pm</option>
+              </select>
+            </>
+          )}
           <button
             className="btn btn-primary mx-auto block w-36"
             onClick={async event => {
