2b3058afa6cc2895c699f1f93f39df2fc2a7592f
Ezzat Chamudi <echamudi@outlook.com>
2024-01-31 22:20:26 +0900

    Add filled URL

--- src/components/screens/nodes/vertical/DataSourceAPIHop.tsx
+++ src/components/screens/nodes/vertical/DataSourceAPIHop.tsx
@@ -210,7 +210,8 @@ export const DataSourceAPIHop = ({
                                 URL
                               </label>
                             </div>
-                            <div className="sm:col-span-2">
+                            <div className="flex items-center sm:col-span-2">
+                              <div className="mr-2 text-sm">flairlabs.ai/companyname/</div>
                               <input
                                 type="text"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
