74c6ddc4be87aea04e8f687f68eb486cb0df6fee
Ezzat Chamudi <echamudi@outlook.com>
2024-02-01 03:26:27 +0900

    Make banner font size small

--- src/components/screens/ResultDetails.tsx
+++ src/components/screens/ResultDetails.tsx
@@ -9,7 +9,7 @@ import { DocLLMOutput, DocWorkflowResult } from 'Types/firebaseStructure'
 import usePaginatedFirestore from '~/lib/usePaginatedFirestore'
 import { ImSpinner9 } from 'react-icons/im'
 import { AiOutlineCopy, AiOutlineExpand } from 'react-icons/ai'
-import { timestampToLocaleString } from './LLMOutputs'
+import { timestampToLocaleDateOnly, timestampToLocaleString } from './LLMOutputs'
 import { v4 as uuidv4 } from 'uuid'
 import { db } from '../../lib/firebase'
 import { useCopyToClipboard } from 'react-use'
@@ -198,32 +198,32 @@ const ResultDetails = ({ id }: { id?: string }) => {
       <header className="stats mb-4 w-full grid-cols-4 border shadow">
         <div className="stat overflow-hidden">
           <div className="stat-title">Model</div>
-          <div className="stat-value">GPT-4</div>
-          {/* <div className="stat-value">{data.model}</div> */}
+          <div className="stat-value text-2xl">GPT-4</div>
+          {/* <div className="stat-value text-2xl">{data.model}</div> */}
         </div>
         <div className="stat overflow-hidden">
           <div className="stat-title">Relevancy</div>
-          <div className="stat-value">94%</div>
-          {/* <div className="stat-value">{averageEval.answer_relevancy?.toFixed(3)}</div> */}
+          <div className="stat-value text-2xl">94%</div>
+          {/* <div className="stat-value text-2xl">{averageEval.answer_relevancy?.toFixed(3)}</div> */}
           {/* <div className="stat-desc">5% more than last run</div> */}
         </div>
         <div className="stat overflow-hidden">
           <div className="stat-title">Hallucination</div>
-          <div className="stat-value">1%</div>
-          {/* <div className="stat-value">-</div> */}
+          <div className="stat-value text-2xl">1%</div>
+          {/* <div className="stat-value text-2xl">-</div> */}
           {/* <div className="stat-desc">21% more than last run</div> */}
         </div>
         <div className="stat overflow-hidden">
           <div className="stat-title">Invalid Format</div>
-          <div className="stat-value">2%</div>
-          {/* <div className="stat-value">{averageEval.invalid_format_percentage || 0}%</div> */}
+          <div className="stat-value text-2xl">2%</div>
+          {/* <div className="stat-value text-2xl">{averageEval.invalid_format_percentage || 0}%</div> */}
           {/* <div className="stat-desc">4% more than last run</div> */}
         </div>
       </header>
       <header className="stats mb-4 w-full grid-cols-4 border shadow">
         <div className="stat overflow-hidden">
-          <div className="stat-title">Request Time</div>
-          <div className="stat-value text-sm">
+          <div className="stat-title">Request Date</div>
+          <div className="stat-value text-2xl">
             {timestampToLocaleString(data.createdTimestamp)}
             {/* <div className="text-3xl">2023/06/25</div>
             <div className="stat-desc text-lg font-bold">10:45:30</div> */}
@@ -231,13 +231,13 @@ const ResultDetails = ({ id }: { id?: string }) => {
         </div>
         <div className="stat overflow-hidden">
           <div className="stat-title">Total Time</div>
-          <div className="stat-value">1h 12min</div>
-          {/* <div className="stat-value">-</div> */}
+          <div className="stat-value text-2xl">1h 12min</div>
+          {/* <div className="stat-value text-2xl">-</div> */}
         </div>
         <div className="stat overflow-hidden">
           <div className="stat-title">Average Tokens</div>
-          <div className="stat-value">56 tokens</div>
-          {/* <div className="stat-value">
+          <div className="stat-value text-2xl">56 tokens</div>
+          {/* <div className="stat-value text-2xl">
             {typeof averageEval.average_tokens_per_request === 'number'
               ? Math.round(averageEval.average_tokens_per_request)
               : '-'}
@@ -246,8 +246,8 @@ const ResultDetails = ({ id }: { id?: string }) => {
         </div>
         <div className="stat overflow-hidden">
           <div className="stat-title">Average Latency</div>
-          <div className="stat-value">200ms </div>
-          {/* <div className="stat-value">
+          <div className="stat-value text-2xl">200ms </div>
+          {/* <div className="stat-value text-2xl">
             {averageEval.average_latency_per_request?.toFixed(3) ?? '-'}
           </div> */}
           {/* <div className="text-lg font-bold stat-desc">Avg: 200ms</div> */}
