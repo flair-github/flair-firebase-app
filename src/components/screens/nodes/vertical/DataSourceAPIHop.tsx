bf2c29c2d43a90cd8bb8e69f3c5795aa544b397e
Ezzat Chamudi <echamudi@outlook.com>
2024-02-05 18:06:17 +0900

    Add test payload

--- src/components/screens/nodes/vertical/DataSourceAPIHop.tsx
+++ src/components/screens/nodes/vertical/DataSourceAPIHop.tsx
@@ -20,6 +20,7 @@ export interface DataSourceAPIHopContent {
   url: string
   method: string
   body: string
+  testPayload: string
 }
 
 export const dataSourceAPIHopDefaultContent: DataSourceAPIHopContent = {
@@ -28,6 +29,7 @@ export const dataSourceAPIHopDefaultContent: DataSourceAPIHopContent = {
   url: '',
   method: 'GET',
   body: '',
+  testPayload: '',
 }
 
 export const DataSourceAPIHop = ({
@@ -285,6 +287,35 @@ export const DataSourceAPIHop = ({
                                   setNodeFormContent(prev => {
                                     const newFormContent = cloneDeep(prev)
                                     newFormContent.body = newText
+                                    return newFormContent
+                                  })
+                                }}
+                              />
+                            </div>
+                          </div>
+
+                          {/* Test Payload */}
+                          <div className="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                            <div>
+                              <label className="block text-sm font-medium leading-6 text-gray-900 sm:mt-1.5">
+                                Test Payload
+                              </label>
+                            </div>
+                            <div className="sm:col-span-2">
+                              <textarea
+                                rows={5}
+                                className="block w-full rounded-md border-0 py-1.5 font-mono text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                value={nodeFormContent.testPayload}
+                                onChange={e => {
+                                  const newText = e.target.value
+
+                                  if (typeof newText !== 'string') {
+                                    return
+                                  }
+
+                                  setNodeFormContent(prev => {
+                                    const newFormContent = cloneDeep(prev)
+                                    newFormContent.testPayload = newText
                                     return newFormContent
                                   })
                                 }}
