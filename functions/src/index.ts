2d338e9cc7dac731d1a31e6cb3f4bd5c3d7a2120
Ezzat Chamudi <echamudi@outlook.com>
2024-02-07 12:19:10 +0900

    Refactor open ai key

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,9 +10,10 @@
 import { HttpsError, onCall } from 'firebase-functions/v2/https'
 import * as logger from 'firebase-functions/logger'
 import OpenAI from 'openai'
+import { OPEN_AI_API_KEY } from './config'
 
 const openai = new OpenAI({
-  apiKey: '', // This is the default and can be omitted
+  apiKey: OPEN_AI_API_KEY, // This is the default and can be omitted
 })
 
 // Start writing functions
