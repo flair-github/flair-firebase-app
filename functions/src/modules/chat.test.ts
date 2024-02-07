f47375bf75030d5d682d817701ca53328b583c42
Ezzat Chamudi <echamudi@outlook.com>
2024-02-07 23:57:07 +0900

    Include pinecone

--- functions/src/modules/chat.test.ts
+++ functions/src/modules/chat.test.ts
@@ -1,5 +1,7 @@
 import { chat } from './chat'
 
+jest.setTimeout(30000)
+
 describe('chat function', () => {
   it('should process messages and return the final chat completion', async () => {
     const res = await chat('Hello')
--- functions/src/modules/chat.ts
+++ functions/src/modules/chat.ts
@@ -1,25 +1,51 @@
 import OpenAI from 'openai'
-import { OPEN_AI_API_KEY } from '../config'
+import { OPEN_AI_API_KEY, PINECONE_API_KEY } from '../config'
+import { Pinecone } from '@pinecone-database/pinecone'
+
+const pc = new Pinecone({
+  apiKey: PINECONE_API_KEY,
+})
 
 const openai = new OpenAI({
   apiKey: OPEN_AI_API_KEY,
 })
 
+const index = pc.Index('faq-docs')
+
 export const chat = async (content: string) => {
+  const embeddingsRes = await openai.embeddings.create({
+    input: content,
+    model: 'text-embedding-3-small',
+  })
+
+  const values = embeddingsRes.data[0].embedding
+
+  const indexQueryRes = index.query({
+    vector: values,
+    topK: 1,
+    includeValues: true,
+    includeMetadata: true,
+  })
+
+  const context = (await indexQueryRes).matches[0].metadata
+
+  const prompt = `
+  Respond to the user query given the following context:
+
+  CONTEXT:
+  question: ${context?.question}
+  answer: ${context?.answer}
+
+  QUERY:
+  ${content}
+`
+
   const stream = await openai.beta.chat.completions.stream({
-    model: 'gpt-4',
-    messages: [{ role: 'user', content }],
+    model: 'gpt-4-1106-preview',
+    messages: [{ role: 'user', content: prompt }],
     stream: true,
+    temperature: 0,
   })
-
-  stream.on('content', (delta, snapshot) => {
-    process.stdout.write(delta)
-  })
-
-  // or, equivalently:
-  for await (const chunk of stream) {
-    process.stdout.write(chunk.choices[0]?.delta?.content || '')
-  }
 
   const chatCompletion = await stream.finalChatCompletion()
 
