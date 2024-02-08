5d10b17c9ff11b69cd35343caf9f101a3c8418a4
Ezzat Chamudi <echamudi@outlook.com>
2024-02-08 20:51:28 +0900

    Remove supabase

--- src/jotai/jotai.tsx
+++ src/jotai/jotai.tsx
@@ -1,14 +1,8 @@
 import { DocUser } from 'Types/firebaseStructure'
 import { User } from 'firebase/auth'
 import { atom, useAtom } from 'jotai'
-import { createClient } from '@supabase/supabase-js'
-import { Database } from '~/supabase'
 
 export const atomUser = atom<User | null | undefined>(undefined)
 export const atomUserData = atom<DocUser | undefined>(undefined)
-export const atomClient = atom(
-  import.meta.env.VITE_SUPABASE_URL && import.meta.env.VITE_SUPABASE_KEY
-    ? createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
-    : undefined,
-)
+
 export const atomNodeExportedKeys = atom<Record<string, Record<string, boolean>>>({})
