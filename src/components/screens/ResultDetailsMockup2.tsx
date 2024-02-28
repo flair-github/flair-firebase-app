53ff3f7029fdebc6e4e7dfa169d7438c0425ddfc
Ezzat Chamudi <echamudi@outlook.com>
2024-02-28 14:52:12 +0700

    Add padding

--- src/components/screens/ResultDetailsMockup2.tsx
+++ src/components/screens/ResultDetailsMockup2.tsx
@@ -408,7 +408,7 @@ const ResultDetailsMockup2 = ({ id }: { id?: string }) => {
             <div className="-mx-4 overflow-x-auto overflow-y-hidden px-4">
               <div className="mt-5 flow-root">
                 <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
-                  <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                  <div className="inline-block min-w-full pb-5 pt-2 align-middle sm:px-6 lg:px-8">
                     <div className="overflow-hidden shadow ring-1 ring-black/5 sm:rounded-lg">
                       <table className="min-w-full divide-y divide-gray-300">
                         <thead className="bg-gray-50">
